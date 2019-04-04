fdc360fb104189139a0792c16452c8071460a8b1
H-Zeus <865291170@qq.com>
2019-04-04 11:23:44 +0800

    2019年4月4日11点22分

---
+++ app/Http/Controllers/wechat/WechatController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WechatController extends Controller
+{
+    /**
+     * @content 微信绑定服务器校验
+     */
+    public function check(Request $request)
+    {
+        dd(1);
+        $signature = $request->signature; //微信加密签名
+        $timestamp = $request->timestamp; //时间戳
+        $nonce = $request->nonce; //随机数
+        if($this->CheckSignature($signature,$timestamp,$nonce)){
+            echo $echostr;exit;
+        }
+    }
+
+    /**
+     * @content 校验微信签名
+     */
+    private function CheckSignature($signature,$timestamp,$nonce)
+    {
+        
+        $token = env('WEIXINTOKEN');
+        $arr = [$token,$timestamp,$nonce];
+        sort($arr,SORT_STRING); //进行字典序排序
+        $str = implode($arr); //拼接成字符串
+        $sign = sha1($str); //进行sha1加密
+        //对比sign
+        if($sign == $signature){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
